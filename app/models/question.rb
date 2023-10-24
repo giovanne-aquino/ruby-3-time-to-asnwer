49a5310f669753cbaa204fdbc3bf316e6eb4bb90
Giovanne <giovanne.aquno.clear@gmail.com>
2023-10-24 11:36:01 -0300

    configure gem cocoon

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
-  belongs_to :subject
+  belongs_to :subject, inverse_of: :questions
   has_many :answers
-  accepts_nested_attributes_for :answers, allow_destroy: true
+  accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
  end
  params = { question: {
   description: 'lorem ipsum?', subject_id: 1,
