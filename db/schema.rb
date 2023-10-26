33e59d25cff80fc5a24854fb00ee92e0c608ed54
Giovanne <giovanne.aquno.clear@gmail.com>
2023-10-26 12:24:25 -0300

    forces user to login and adds user statistics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_26_145056) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_151824) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,6 +85,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_145056) do
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "right_questions"
+    t.integer "wrong_questions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,4 +113,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_26_145056) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "subjects"
   add_foreign_key "user_profiles", "users"
+  add_foreign_key "user_statistics", "users"
 end
