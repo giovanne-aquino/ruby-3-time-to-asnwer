8ee7a3428c47ed07f596279e12fe6c7005ceee8e
Giovanne <giovanne.aquno.clear@gmail.com>
2023-10-26 08:40:30 -0300

    use counter_cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_23_150853) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_111106) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_23_150853) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
